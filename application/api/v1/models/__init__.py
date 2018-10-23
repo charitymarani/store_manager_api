08afe9b52d2ba83786d4f804c893bf55f60f2d54
Charity Marani <marani.charity95@gmail.com>
2018-10-23 10:19:57 +0300

    Update __init__.py

--- application/api/v1/models/__init__.py
+++ application/api/v1/models/__init__.py
@@ -1,3 +1,3 @@
-from .auth_models import USERS_DICT
-from .sales_model import SALES_DICT
-from .product_model import PRODUCTS_DICT
+from .auth_models import USERS_LIST
+from .sales_model import SALES_LIST
+from .product_model import PRODUCTS_LIST
