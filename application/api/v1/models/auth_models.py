3f2cc2120f340f81e64ff462a2c5aa90ec1e0d73
Charity Marani <marani.charity95@gmail.com>
2018-10-23 10:08:06 +0300

    Update auth_models.py

--- application/api/v1/models/auth_models.py
+++ application/api/v1/models/auth_models.py
@@ -3,18 +3,18 @@ from flask import jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from ..utils import get_item_by_key, get_all
 
-USERS_DICT = []
+USERS_LIST = []
 
 
 class Users():
     '''class to represent users model'''
 
     def put(self, name, username, email, password, role):
-        '''add a user to USERS_DICT'''
+        '''add a user to USERS_LIST'''
         self.oneuser_dict = {}
 
-        email_data = get_item_by_key("email", email, USERS_DICT)
-        username_data = get_item_by_key("username", username, USERS_DICT)
+        email_data = get_item_by_key("email", email, USERS_LIST)
+        username_data = get_item_by_key("username", username, USERS_LIST)
         if "message" not in email_data:
             return {"message": "Email already in use,try a different one!"}
         if "message" not in username_data:
@@ -27,12 +27,12 @@ class Users():
         pw_hash = generate_password_hash(password)
         self.oneuser_dict["password"] = pw_hash
 
-        USERS_DICT.append(self.oneuser_dict)
+        USERS_LIST.append(self.oneuser_dict)
         return {"message": "User with username {} added successfully".format(username)}
 
     def verify_password(self, username, password):
         '''verify the password a user enters while logging in'''
-        user_obj = get_item_by_key('username', username, USERS_DICT)
+        user_obj = get_item_by_key('username', username, USERS_LIST)
         if "message" not in user_obj:
             result = check_password_hash(
                 user_obj['password'], password)
@@ -42,9 +42,9 @@ class Users():
         return user_obj
 
     def get_user_by_username(self, username):
-        result = get_item_by_key('username', username, USERS_DICT)
+        result = get_item_by_key('username', username, USERS_LIST)
         return result
 
     def get_all_users(self):
-        result = get_all(USERS_DICT)
+        result = get_all(USERS_LIST)
         return result
